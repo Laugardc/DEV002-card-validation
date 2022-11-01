990f71a4a8ab29c591455cbd0f549d07d623e6b3
laugardc <laugardc@gmail.com>
2022-10-31 23:58:49 -0600

    CSS, html, pseudocodigo

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,29 @@
 const validator = {
-  // ...
-};
+  isValid: function (cardNumber){
+    split().map(cardNumber)//string a array
+    return newNumberArray(str)
+    let newNumberArray;
+    for (i = 0; i < cardNumber.length; i++){//posiciones pares
+      if(i % 2 ===0) { 
+        do(i*2)//multiplicar por dos
+        while (i>9) slice [0-1] (i=0 + i=1)// mientras sea mayor a 9, divide y suma sus digitos
+      }
+      else {
+
+      }//Si son las posiciones impares, dejalos igual
+
+    } // Recorre el arreglo
+
+  //for (let i = 0; i < arr.length; i += 2) {// el string, se convierte en array con el for, aqui solo me va a dar las posiciones pares dentro del array, lo debo correr de atras hacia adelante
+      //;} // doblar los digitos dentro de las posiciones pares del array, 
+      ////si el numero que arroja es igual (0,1,2,3,4,5,6,7,8,9) dejalo pasar para sumarlo
+      ////que los numeros >9 dividelos en dos digitos para sumarlos
+
+
+
+
+  }
+
+},
 
 export default validator;
