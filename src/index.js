9dfecf85291241c8ed7e9f09ef2be2bcb14c35a6
laugardc <laugardc@gmail.com>
2022-11-04 19:33:22 -0600

    Avance index, click Validator isValid, sirve

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import validator from './validator.js';
-//isValid()
 
-//console.log(validator);
-//const btnIsValid = document.getElementById("btn");
-//btnIsValid.addEventListener("click", validator);
-//console.log(btnIsValid);
-validator.isValid('123456789012334');
+const buttonValidator = document.getElementById("btn");//Traigo al elemento button de html
+const inputValidator = document.getElementById("inputNumero");//Traigo al elemento input numero
+
+//console.log(buttonValidator);
+//console.log(inputValidator);
+
+buttonValidator.addEventListener('click', function () {//voy a agregar un evento 'click' a mi button
+    const isValid = validator.isValid(inputValidator.value);//para que cuando suceda el eventl, del valor de mi input numero corra la funcion isValid que esta dentro de validator.
+    if (isValid === true){
+        alert('Tarjeta valida');//si es true mi validator, va a arrojarme alerta Tarjeta valida
+    } else {
+        alert('Tarjeta invalida');//sino, tarjeta invalida
+    }
+});
+
+
