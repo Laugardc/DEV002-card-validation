be17a73f1598510e74a41ea7bf4ae0064b89738c
laugardc <laugardc@gmail.com>
2022-11-07 15:47:56 -0600

    Solo permito numeros y #

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import validator from './validator.js';
 const buttonValidator = document.getElementById("btn");//Traigo al elemento button de html
 const inputValidator = document.getElementById("inputNumero");//Traigo al elemento input numero
 
-//console.log(buttonValidator);
-//console.log(inputValidator);
-
 buttonValidator.addEventListener('click', function () {//voy a agregar un evento 'click' a mi button
     const isValid = validator.isValid(inputValidator.value);//para que cuando suceda el eventl, del valor de mi input numero corra la funcion isValid que esta dentro de validator.
     if (isValid === true){
@@ -15,3 +12,13 @@ buttonValidator.addEventListener('click', function () {//voy a agregar un evento
     }
 });
 
+inputValidator.addEventListener('keyup', function () {
+
+    const stringMaskify = validator.maskify(inputValidator.value);
+    console.log(stringMaskify);
+    console.log('keyup');
+});
+
+inputValidator.addEventListener('input', function (){
+    this.value = this.value.replace(/[^0-9#]/g, '').replace(/(\..*)\./g, '$1');
+});
